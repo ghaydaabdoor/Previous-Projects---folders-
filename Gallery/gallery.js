6a8c01ce095bceb3bc6992a608ce895d364b58c7
ghaydaabdoor <ghaydaa.bdoor@outlook.com>
2024-05-19 08:36:47 +0300

    19 May

---
+++ Gallery/gallery.js
@@ -0,0 +1,35 @@
+let bigimg= document.getElementById('bigimg')
+let pic1= document.getElementById('pic1')
+let pic2= document.getElementById('pic2')
+let pic3= document.getElementById('pic3')
+let pic4= document.getElementById('pic4')
+let pic11= document.getElementById('pic11')
+let pic22= document.getElementById('pic22')
+let pic33= document.getElementById('pic33')
+let pic44= document.getElementById('pic44')
+pic1.onclick= function(){
+	bigimg.src=pic1.src;
+}
+pic2.onclick= function(){
+	bigimg.src=pic2.src;
+}
+pic3.onclick= function(){
+	bigimg.src=pic3.src;
+}
+pic4.onclick= function(){
+	bigimg.src=pic4.src;
+}
+pic11.onmouseover= function(){
+	bigimg.src=pic11.src;
+}
+pic22.onmouseover= function(){
+	bigimg.src=pic22.src;
+}
+pic33.onmouseover= function(){
+	bigimg.src=pic33.src;
+}
+pic44.onmouseover= function(){
+	bigimg.src=pic44.src;
+}
+
+
